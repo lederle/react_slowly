e1687c9beb3fbcfabf9e20af2ad30bfc0b41d9c4
mike lederle <mrlederle@gmail.com>
2024-10-21 13:47:54 -0400

    copy paste links

--- rq02-custom-links/src/main.jsx
+++ rq02-custom-links/src/main.jsx
@@ -2,4 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render()
+const link1 = React.createElement("a", { href : "\\react.dev"}, "read more about react");
+const p1 = React.createElement("p", null, link1);
+const link2 = React.createElement("a", { href : "\\react.dev"}, "read more about react");
+const p2 = React.createElement("p", null, link1);
+const link3 = React.createElement("a", { href : "\\react.dev"}, "read more about react");
+const p3 = React.createElement("p", null, link1);
+
+const group = React.createElement(React.Fragment, null, p1, p2, p3);
+ReactDOM.createRoot(document.getElementById('root')).render(group);
