32434a4916cee44a3a67d7ff6579437609ddd9d0
mike lederle <mrlederle@gmail.com>
2024-10-21 17:34:17 -0400

    Links using children property

--- rq02-links-children/src/main.jsx
+++ rq02-links-children/src/main.jsx
@@ -2,4 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render()
+class Link extends React.Component {
+  render() {
+    const link = React.createElement("a", { href: this.props.url}, this.props.children);
+    return React.createElement("p", null, link); 
+  }
+}
+
+const boldReact = React.createElement("strong", null , "react");
+const link1 = React.createElement(Link, { url: "//react.dev"}, boldReact);
+const link2 = React.createElement(Link, { url: "//vuejs.org"}, "vue");
+const link3 = React.createElement(Link, { url: "//angular.dev"}, "angular");
+
+const group = React.createElement(React.Fragment, null, link1, link2, link3);
+ReactDOM.createRoot(document.getElementById('root')).render(group);
