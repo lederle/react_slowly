3998c032376edf9f0c87159ddd93cbd9e20f7c0e
mike lederle <mrlederle@gmail.com>
2024-10-21 14:33:47 -0400

    Use React.Component

--- rq02-custom-links/src/main.jsx
+++ rq02-custom-links/src/main.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-const link1 = React.createElement("a", { href : "\\react.dev"}, "read more about react");
-const p1 = React.createElement("p", null, link1);
-const link2 = React.createElement("a", { href : "\\react.dev"}, "read more about react");
-const p2 = React.createElement("p", null, link1);
-const link3 = React.createElement("a", { href : "\\react.dev"}, "read more about react");
-const p3 = React.createElement("p", null, link1);
+class Link extends React.Component {
+  render() {
+    const link = React.createElement("a", { href : "\\react.dev"}, "Read more about react");
+    return React.createElement("p", null , link);
+  }
+}
 
-const group = React.createElement(React.Fragment, null, p1, p2, p3);
+const link1 = React.createElement(Link);
+const link2 = React.createElement(Link);
+const link3 = React.createElement(Link);
+
+const group = React.createElement(React.Fragment, null, link1, link2, link3);
 ReactDOM.createRoot(document.getElementById('root')).render(group);
