c5b1d8578b57a01f4a3a853d1b5c7bcb548b6fbb
Mike Lederle <mrlederle@gmail.com>
2024-10-21 18:30:06 -0400

    Use App component (#8)

---
+++ rq02-links-app/src/App.jsx
@@ -0,0 +1,40 @@
+import React, { Fragment, Component } from "react";
+import './App.css'
+
+class Link extends Component {
+  render() {
+    return React.createElement(
+      "p",
+      null,
+      React.createElement(
+        "a",
+        { href: this.props.url },
+        `Read more about ${this.props.framework}`
+      )
+    )
+  }
+}
+class App extends Component {
+  render() {
+    const link1 = React.createElement(Link, {
+      framework: "React",
+      url: "//react.dev",
+    });
+
+    const link2 = React.createElement(Link, {
+      framework: "Vue",
+      url: "//vuejs.org",
+    });
+
+    const link3 = React.createElement(Link, {
+      framework: "Angular",
+      url: "//angular.dev",
+    });
+
+    return React.createElement(
+      Fragment, null, link1, link2, link3
+    );
+  }
+}
+
+export default App;
